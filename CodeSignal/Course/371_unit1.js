ff039ae41b2f413abb7e62b5dbf3d0f9d25805bd
Leng Yang <lengyang@Lengs-Mac-Studio.local>
2025-01-12 23:40:19 -0600

    start CodeSignal lession 371

---
+++ CodeSignal/Course/371_unit1.js
@@ -0,0 +1,21 @@
+class CafeteriaStack {
+  constructor() {
+      this.stack = [];
+  }
+
+  addTray(trayId) {
+      this.stack.push(trayId);
+  }
+
+  removeTray() {
+      if (this.stack.length === 0) {  // Simplified check for an empty stack
+          return this.stack.pop();
+      }
+  }
+}
+
+// Sample usage
+const cafeteria = new CafeteriaStack();
+cafeteria.addTray("Tray_4");  // Adding a tray to the stack
+console.log(cafeteria.removeTray());  // This should print "Tray_4"
+console.log(cafeteria.removeTray());  // Prints: "No more trays!"
